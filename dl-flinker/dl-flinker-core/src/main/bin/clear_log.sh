b40ec4685e3c106b97f55ec88cf80ab5ff957677
songwenbinasd <993036091@qq.com>
2020-02-21 15:40:57 +0800

    提交有误

--- dl-flinker/dl-flinker-core/src/main/bin/clear_log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-current_date=`date +%Y%m01`
-three_months_ago=`date -d "${current_date} last month last month last month" +%Y-%m`
-
-logs_path="/usr/local/frame/dl-manager/logs/manager"
-rm -rf $logs_path"/"$three_months_ago"*"
-
--- dl-flinker/dl-flinker-sqlserverreader/src/main/java/com/ucar/datalink/flinker/plugin/reader/sqlserverreader/Key.java
+++
@@ -1,6 +0,0 @@
-package com.ucar.datalink.flinker.plugin.reader.sqlserverreader;
-
-public class Key {
-
-    public static final String FETCH_SIZE = "fetchSize";
-}
